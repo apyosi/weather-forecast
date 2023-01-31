7afc20b4ba3eb0bbfb0b655d55f93207c1c27bd6
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-31 18:50:49 +0200

    add fetch function for search input

--- index.js
+++ index.js
@@ -16,3 +16,33 @@ let urlForecast5 = `https://api.openweathermap.org/data/2.5/forecast?q=london&ap
 
 
 searchButton.addEventListener("click", fetchGeocoding);
+
+function fetchGeocoding(event) {
+  event.preventDefault();
+  clearResults();
+  let searchWord = searchInput.value;
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${searchWord}&limit=5&appid=${apiKey}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let name = data[i].name;
+        let country = data[i].country;
+        let state = data[i].state;
+        let lat = data[i].lat;
+        let lon = data[i].lon;
+
+        let li = document.createElement("li");
+        if (state) {
+          li.innerHTML = `<li><a href="#" lat="${lat}" lon="${lon}" title="lat:${lat} lon:${lon}" >${name}, ${country}, ${state}</a></li>`;
+        } else {
+          li.innerHTML = `<li><a href="#"  lat="${lat}" lon="${lon} title="lat:${lat} lon:${lon}"">${name}, ${country}</a></li>`;
+        }
+        // searchResultEl.innerHTML +=  li;
+        searchResultList.appendChild(li);
+      }
+      searchResults.innerHTML = `<h3>Results: ${data.length}</h3>`;
+    });
+}
