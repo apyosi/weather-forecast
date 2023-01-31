e5a00b962deb72848f161b35686985ee94d8b279
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-31 18:43:36 +0200

    Add API key to use openweathermap.org free data

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ const today = document.querySelector("#today");
 const forecast = document.querySelector(".forecast");
 const fiveDay = document.querySelector("#five-day");
 
+//API key to use openweathermap.org free weather data
+let apiKey = "6008f3fcaf990bc7c0beb645fd2a3fb3";
+let urlGeocoding = `https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${apiKey}`;
+let urlForecast5 = `https://api.openweathermap.org/data/2.5/forecast?q=london&appid=${apiKey}`;
