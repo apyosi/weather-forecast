d7454cc943813f091736b7ebfd0cc4e7bfea9156
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-31 19:16:43 +0200

    add function to get current location

--- script.js
+++ script.js
@@ -52,3 +52,33 @@ function clearResults() {
   searchResultList.innerHTML = "";
 }
 
+function currentLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  fetchCurrentWeather(lat, lon);
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      alert("User denied the request for Geolocation.")
+      break;
+    case error.POSITION_UNAVAILABLE:
+      alert("Location information is unavailable.")
+      break;
+    case error.TIMEOUT:
+      alert("The request to get user location timed out.")
+      break;
+    case error.UNKNOWN_ERROR:
+      alert("An unknown error occurred.")
+      break;
+  }
+}
