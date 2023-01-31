db0117bf7efb238a42bbfb533c6d167aa06b23fd
Anton Yosifov <yosifov.anton@gmail.com>
2023-01-31 18:39:37 +0200

    Select html elements with document.querySelector

---
+++ index.js
@@ -0,0 +1,11 @@
+// Select html elements with document.querySelector
+const searchInput = document.querySelector("#search-input");
+const searchButton = document.querySelector("#search-button");
+const searchResults = document.querySelector("#search-results");
+const searchResultList = document.querySelector("#search-results-list");
+const formSelect = document.querySelector(".form-select");
+const historyEl = document.querySelector("#history");
+const today = document.querySelector("#today");
+const forecast = document.querySelector(".forecast");
+const fiveDay = document.querySelector("#five-day");
+
